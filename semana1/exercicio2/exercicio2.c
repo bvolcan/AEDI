dda341ed27098425eed378dac66cfb4c6713f71f
bvolcan <brunovolcan@gmail.com>
2021-04-02 20:43:48 -0300

    brief documentation

--- semana1/exercicio2/exercicio2.c
+++ semana1/exercicio2/exercicio2.c
@@ -52,7 +52,7 @@ void insert()
 {
     fgetc(stdin);
     printf("Insira o nome: ");
-    if (listSize != 0)
+    if (listSize != 0) // Separação dos nomes por ", "
     {
         nameList[listSize] = ',';
         listSize++;
@@ -91,20 +91,20 @@ void removeName()
     int i, sizeCounter;
     for (i = 0; i < listSize && nameList[i] != '\0'; i++)
     {
-        if (i <= 2 || nameList[i - 2] == ',')
+        if (i <= 2 || nameList[i - 2] == ',') // Teste para saber se se trata do primeiro nome ou não.
         {
-            if (i <= 2)
+            if (i <= 2) // Caso seja o primeiro nome garante que a variável a ser utilizada seja 0
             {
                 sizeCounter = 0;
             }else{
                 sizeCounter = i;
             }
             
-            for (; nameList[sizeCounter] != ',' && nameList[sizeCounter] != '\0'; sizeCounter++);
+            for (; nameList[sizeCounter] != ',' && nameList[sizeCounter] != '\0'; sizeCounter++); // Conta o tamanho do nome na lista
              if ((sizeCounter - i) == inputNameSize || sizeCounter == inputNameSize)
             {
                 int j, k, l;
-                if (i <= 2)
+                if (i <= 2) // Caso seja o primeiro nome garante que aa variáveis a serem utilizadas sejam 0
                 {
                     j = 0;
                     l = 0;
@@ -113,20 +113,20 @@ void removeName()
                     l = i;
                 }
 
-                for (k = 0; nameList[j] == inputName[k] && k < (sizeCounter - l); j++, k++);
+                for (k = 0; nameList[j] == inputName[k] && k < (sizeCounter - l); j++, k++); // Compara os nomes de letra em letra
                 if (k == inputNameSize)
                 {
-                    if((i == 0 || i <= 2) && !(listSize > inputNameSize)) {
+                    if((i == 0 || i <= 2) && !(listSize > inputNameSize)) { // Testa se é o primeiro e único nome na lista
                         listSize = 0;
                         nameList = realloc(nameList, sizeof(char));
                         nameList[0] = '\0';
-                    }else if (nameList[sizeCounter] == '\0')
+                    }else if (nameList[sizeCounter] == '\0') // Testa se é o último nome da lista
                     {
                         listSize -= inputNameSize + 2;
                         nameList = realloc(nameList, sizeof(char) * (listSize + 1));
                         nameList[listSize] = '\0'; 
                     }else {
-                    for (k = i; nameList[k + inputNameSize + 1] != '\0'; k++)
+                    for (k = i; nameList[k + inputNameSize + 1] != '\0'; k++) // Switch para a situação em que é um nome no meio da lista
                     {
                         nameList[k] = nameList[k + inputNameSize + 2];
                     }
